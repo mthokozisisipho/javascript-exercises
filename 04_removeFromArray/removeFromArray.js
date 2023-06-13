690d7c1eacb5b08e2c82b840a58e30fbe789c30b
mthokozisisipho <122373617+mthokozisisipho@users.noreply.github.com>
2023-06-13 20:37:03 +0200

    Add comments on fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
 const removeFromArray = function(myArray) {
+    // Iterate through the arguments if there are any
     for (let i = 1; i < arguments.length; i++) {
+        /* On each argument find out where it is located in myArray
+           And store that index place */
         let argIndex = myArray.indexOf(arguments[i]);
+
+        /* Now that we know where the argument is stored in the array 
+           we can get to it and remove it from the array */
         if (argIndex in myArray) {
             myArray.splice(argIndex, 1);
         }
