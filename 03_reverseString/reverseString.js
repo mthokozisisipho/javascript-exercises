69d8a442f848c5d7530d5931312427e07175e487
mthokozisisipho <122373617+mthokozisisipho@users.noreply.github.com>
2023-06-13 17:42:59 +0200

    Solve the third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const chars = string.split("");
+    let newString = "";
+    for (let i = chars.length - 1; i >= 0; i--) {
+        newString += chars[i];
+    }
+    return newString;
 };
 
 // Do not edit below this line
