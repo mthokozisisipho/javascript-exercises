c880cb9e85de221de3acbdfc263532fc45b8d00a
mthokozisisipho <122373617+mthokozisisipho@users.noreply.github.com>
2023-06-13 20:24:26 +0200

    Solve the fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray) {
+    for (let i = 1; i < arguments.length; i++) {
+        let argIndex = myArray.indexOf(arguments[i]);
+        if (argIndex in myArray) {
+            myArray.splice(argIndex, 1);
+        }
+    }
+    return myArray;
 };
 
 // Do not edit below this line
